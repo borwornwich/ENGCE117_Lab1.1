76881edc85f6328f9c2c9d248da4a2d4d4084dd9
borwornwich <117151492+borwornwich@users.noreply.github.com>
2023-12-11 13:02:05 +0700

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int N ;
+	int i ;
+	
+	printf( "Input Number : " ) ;
+	scanf( "%d", &N ) ;
+	
+	for( i = 1 ; i <= N ; i++ ) {
+		printf( "[%d] Hellow world \n" ,i) ;
+	}
+}
